1f92dd9f4ee80a43449725c27d688b0722b2daee
Peter Krevenets <peter.k.nets@gmail.com>
2016-12-08 14:44:32 +0200

    initial commit

---
+++ src/scripts/hello.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Hello extends React.Component {
+  render() {
+    return <h1>Hello</h1>
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(<Hello />, document.getElementById('hello'));
+});
